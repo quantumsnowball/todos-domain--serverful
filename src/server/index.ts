512b136d50c20d28933f53f8ec5e516892b925e4
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-09 00:57:47 +0800

    refactor: express already has body-parser builtin

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,36 +2,54 @@ import express, { Request, Response } from 'express'
 import path from 'path'
 import dotenv from 'dotenv'
 import history from 'connect-history-api-fallback'
-import bodyParser from 'body-parser'
 
-
-// configs
+//
+// Configs
+//
+// .env
 dotenv.config()
+const port_server = process.env.PORT_SERVER || 3000
+// routes
 const app = express()
 const api = express.Router()
-const port_server = process.env.PORT_SERVER || 3000
 app.use(history())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
+//
 // static serving
+//
 app.use(express.static('dist'))
 
-// default
-app.get('*', function(_: Request, res: Response) {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
+//
 // paths
+//
 api.route('/hello').get((_: Request, res: Response) => {
     res.send('Hello World! Greeting from Express JS.')
 })
 
 api.post('/login', (req: Request, res: Response) => {
-    res.send(`Hello User! You are logging in as ${req.body.email}`)
+    const { email, password } = req.body
+    if (userIsValid(email, password)) {
+        res.status(200).send(`You are logging in as ${email}`)
+    } else {
+        res.status(401).send(`User ${email} does not exist, please try again`)
+    }
 })
 
 app.use('/api', api)
 
+// 
+// port
+//
 app.listen(port_server, () => {
     console.log(`Express server listening on port ${port_server}`)
 })
+
+//
+// dev dummy simulation
+//
+const userIsValid = (email: string, password: string) =>
+    (email === 'a@b.c' && password === 'w')
+
