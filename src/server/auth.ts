57520e40aed76b7de060a78757bdf5467441ebe5
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-10 02:48:39 +0800

    refactor: as middleware

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,24 +1,62 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import { User } from './types'
+import { RequestHandler } from 'express'
 
 
 //
 // dev dummy simulation
 //
-export const userIsAuthorized = async (email: string, password: string, users: User[]) => {
-    const user = users.find(user => user.email === email)
-    console.log({ users })
-    if (!user)
-        return false
-    const result = await bcrypt.compare(password, user.hashedPassword)
-    console.log({ result })
-    return result
+// dummy user database
+const users: User[] = []
+
+// middleware
+export const checkIfUserAlreadyExists: RequestHandler = (req, res, next) => {
+    const userFound = users.find(user => user.email === req.body.email)
+    if (userFound)
+        return res.status(406).json({
+            message: `Email address ${userFound.email} has already been taken, please use another email address.`
+        })
+    next()
 }
 
-export const userAlreadyExists = (email: string, users: User[]) =>
-    users.some(user => user.email === email)
+export const registerUserToDatabase: RequestHandler = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+        users.push({ email, hashedPassword })
+        return res.status(200).json({ message: `User ${email} is registered successfully.` })
+    } catch (error) {
+        return res.status(406).json({ message: error })
+    }
+}
 
+export const checkUserEmailPassword: RequestHandler = async (req, res, next) => {
+    // check email
+    const user = users.find(user => user.email === req.body.email)
+    if (!user)
+        return res.status(401).json({
+            message: `User ${req.body.email} does not exist, please try again.`
+        })
+    // check password
+    const passwordMatch = await bcrypt.compare(req.body.password, user.hashedPassword)
+    if (!passwordMatch)
+        return res.status(401).json({
+            message: `Your password is incorrect.`
+        })
+    // all checks passed
+    next()
+}
+
+export const signToken: RequestHandler = async (req, res) => {
+    const { email } = req.body
+    const { accessToken, refreshToken } = signAccessToken({ id: Date.now(), username: email })
+    return res.json({
+        message: `Logged in as ${email}`,
+        accessToken,
+        refreshToken
+    })
+}
 
 // JWT helpers
 export const signAccessToken = (payload: any) => {
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
 import history from 'connect-history-api-fallback'
-import bcrypt from 'bcrypt'
 import { User } from './types'
 import {
-    userAlreadyExists,
-    userIsAuthorized,
-    signAccessToken
+    checkIfUserAlreadyExists,
+    registerUserToDatabase,
+    checkUserEmailPassword,
+    signToken
 } from './auth'
 
 //
@@ -43,45 +43,9 @@ api.route('/hello').get((_: Request, res: Response) => {
     })
 })
 
-api.post('/register', async (req: Request, res: Response) => {
-    const { email, password } = req.body
+api.post('/register', checkIfUserAlreadyExists, registerUserToDatabase)
 
-    if (userAlreadyExists(email, users)) {
-        return res.status(406).json({
-            message: `Email address ${email} has already been taken, please use another email address.`
-        })
-    }
-
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10)
-        users.push({ email, hashedPassword })
-        return res.status(200).json({
-            message: `User ${email} is registered successfully.`
-        })
-    } catch (error) {
-        return res.status(406).json({ message: error })
-    }
-})
-
-api.post('/login', async (req: Request, res: Response) => {
-    const { email, password } = req.body
-
-    if (!await userIsAuthorized(email, password, users)) {
-        return res.status(401).json({
-            message: `User ${email} does not exist, please try again.`
-        })
-    }
-
-    const { accessToken, refreshToken } = signAccessToken({
-        id: Date.now(),
-        username: email
-    })
-    return res.json({
-        message: `Logged in as ${email}`,
-        accessToken,
-        refreshToken
-    })
-})
+api.post('/login', checkUserEmailPassword, signToken)
 
 app.use('/api', api)
 
