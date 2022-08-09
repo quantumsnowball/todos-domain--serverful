4457a37593cc5ef0c325340b5ae23dbb5db327e3
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-09 23:16:10 +0800

    feat: register page, dummy user database, simulated register and login flow

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
 import history from 'connect-history-api-fallback'
 import jwt from 'jsonwebtoken'
+import { User } from './types'
 
 //
 // Configs
@@ -29,17 +30,33 @@ api.route('/hello').get((_: Request, res: Response) => {
     res.send('Hello World! Greeting from Express JS.')
 })
 
+api.post('/register', (req: Request, res: Response) => {
+    const { email, password } = req.body
+    try {
+        users.push({ email, password })
+        res.status(200).json({
+            message: `User ${email} is registered successfully`
+        })
+    } catch (error) {
+        res.status(406).json({ message: error })
+    }
+})
+
 api.post('/login', (req: Request, res: Response) => {
     const { email, password } = req.body
-    if (!userIsValid(email, password))
+
+    if (!userIsValid(email, password)) {
         res.status(401).json({
             message: `User ${email} does not exist, please try again`
         })
+        return
+    }
+
     const { accessToken, refreshToken } = signAccessToken({
         id: Date.now(),
         username: email
     })
-    res.status(200).json({
+    res.json({
         message: `Logged in as ${email}`,
         accessToken,
         refreshToken
@@ -58,8 +75,12 @@ app.listen(port_server, () => {
 //
 // dev dummy simulation
 //
+// dummy user database
+const users: User[] = []
+
 const userIsValid = (email: string, password: string) =>
-    (email === 'a@b.c' && password === 'w')
+    users.some(user => user.email === email && user.password == password)
+
 
 // JWT helpers
 const signAccessToken = (payload: any) => {
