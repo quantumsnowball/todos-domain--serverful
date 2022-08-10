34e9c4e2406d7f8886c14d235b9674b66a8f9ce4
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-11 01:54:08 +0800

    feat: Homepage show the todo list if user is logged in

--- src/client/components/Main/Pages/Homepage.tsx
+++ src/client/components/Main/Pages/Homepage.tsx
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom'
+import { RootState } from '../../../redux/store'
+import { useSelector } from 'react-redux'
 
 
 export default function Homepage() {
+  const refreshToken = useSelector((s: RootState) => s.token.refreshToken)
+
   return (
     <div>
-      <h1>Please <Link to="/login">Login</Link> to view your Todo list. </h1>
+      {refreshToken ?
+        <h1>Fetch and show your todo list.</h1>
+        :
+        <h1>Please <Link to="/login">Login</Link> to view your Todo list. </h1>}
     </div>
   )
 }
