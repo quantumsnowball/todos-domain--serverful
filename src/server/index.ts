fad338b9fb88cfb7027345dc46bd3df42dd6b444
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-14 21:56:46 +0800

    feat: connected to mongodb

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,9 +5,15 @@ import app from './app'
 
 dotenv.config()
 const port_server = process.env.PORT_SERVER || 3000
+const host_mongo = process.env.HOST_MONGO
+const port_mongo = process.env.PORT_MONGO
 
-
-app.listen(port_server, () => {
-  console.log(`Express server listening on port ${port_server}`)
+// connect to mongo
+mongoose.connect(`mongodb://${host_mongo}:${port_mongo}`).then(() => {
+  console.log('Connected to MongoDB')
+  // startt express server
+  app.listen(port_server, () => {
+    console.log(`Express server listening on port ${port_server}`)
+  })
 })
 
